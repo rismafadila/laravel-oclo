c3e29b028d8ba3bbe3f7071517e4641aeff2ae3a
rismafadila <rismaf651@gmail.com>
2021-11-16 06:08:33 +0700

    migration

---
+++ database/migrations/2021_11_15_093049_create_admin_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('gender');
+            $table->Integer('no');
+            $table->string('alamat');
+            $table->foreign('nama')->references('id')
+            ->on('admins')->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_details');
+    }
+}
